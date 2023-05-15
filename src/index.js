a6df42e3d101575b817f7b0dcd210b83d62a22e8
victormc13 <viktorsb14@gmail.com>
2023-05-15 10:00:09 -0500

    Add renderTasks function and an event listener for the form input

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import '../src/style.css';
 
-const tasks = [
-  {
-    description: "Buy some food",
-    completed: true,
-    index: 1,
-  },
-  {
-    description: "Go for walk",
-    completed: false,
-    index: 2,
-  },
-  {
-    description: "Do laundry",
-    completed: false,
-    index: 3,
-  },
-];
+const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-const renderTasks = () => {
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function renderTasks() {
   const taskContainer = document.querySelector(".task-container");
   taskContainer.innerHTML = "";
 
@@ -34,20 +22,74 @@ const renderTasks = () => {
 
     const taskCheckbox = document.createElement("input");
     taskCheckbox.type = "checkbox";
+    taskCheckbox.checked = task.completed;
+    taskCheckbox.addEventListener("change", () => {
+      task.completed = taskCheckbox.checked;
+      saveTasks();
+      renderTasks();
+    });
     taskItem.appendChild(taskCheckbox);
 
     const taskDescription = document.createElement("p");
     taskDescription.innerText = task.description;
+    taskDescription.contentEditable = true;
+    taskDescription.addEventListener("input", () => {
+      task.description = taskDescription.innerText;
+      saveTasks();
+    });
     taskItem.appendChild(taskDescription);
 
     const taskMenu = document.createElement("i");
     taskMenu.classList.add("las", "la-ellipsis-v", "btn");
+    taskMenu.addEventListener("click", () => {
+      const index = tasks.indexOf(task);
+      if (index > -1) {
+        tasks.splice(index, 1);
+        saveTasks();
+        // loop through remaining tasks and update their indexes
+        tasks.forEach((task, index) => {
+          task.index = index;
+        });
+        renderTasks();
+      }
+    });
     taskItem.appendChild(taskMenu);
 
     taskContainer.appendChild(taskItem);
   });
 }
 
-window.addEventListener("load", () => {
+renderTasks();
+
+const form = document.querySelector(".task-form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = form.querySelector("input[type=text]");
+  const description = input.value.trim();
+  if (description) {
+    const index = tasks.length;
+    tasks.push({
+      description,
+      completed: false,
+      index,
+    });
+    saveTasks();
+    renderTasks();
+    input.value = "";
+  }
+});
+
+const clearButton = document.querySelector(".completed-tasks-btn");
+clearButton.addEventListener("click", () => {
+  tasks.forEach((task, index) => {
+    if (task.completed) {
+      tasks.splice(index, 1);
+    }
+  });
+  tasks.forEach((task, index) => {
+    task.index = index;
+  });
+  saveTasks();
   renderTasks();
 });
+
