78d77c6d63f5f6ec1a94162965282e5947187612
victormc13 <viktorsb14@gmail.com>
2023-05-15 01:20:46 -0500

    Add the ToDo list structure and the renderTasks arrow function

--- src/index.js
+++ src/index.js
@@ -1,51 +1,53 @@
-import './style.css';
+import '../src/style.css';
 
 const tasks = [
-  { description: 'task1', completed: false, index: 1 },
-  { description: 'task2', completed: false, index: 2 },
-  { description: 'task3', completed: false, index: 3 },
-  { description: 'task4', completed: false, index: 4 },
+  {
+    description: "Buy some food",
+    completed: true,
+    index: 1,
+  },
+  {
+    description: "Go for walk",
+    completed: false,
+    index: 2,
+  },
+  {
+    description: "Do laundry",
+    completed: false,
+    index: 3,
+  },
 ];
 
-const createTaskItem = (task) => {
-  const li = document.createElement('li');
-  li.classList.add('task-item', 'flex-row');
+const renderTasks = () => {
+  const taskContainer = document.querySelector(".task-container");
+  taskContainer.innerHTML = "";
 
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.addEventListener('change', () => {
-    if (checkbox.checked === true) {
-      li.classList.add('task-completed');
-      task.completed = true;
-    } else {
-      li.classList.remove('task-completed');
-      task.completed = false;
-    }
-  });
-
-  li.appendChild(checkbox);
-
-  const p = document.createElement('p');
-  p.textContent = task.description;
-  li.appendChild(p);
-
-  const icon = document.createElement('i');
-  icon.classList.add('las', 'la-ellipsis-v', 'btn');
-  li.appendChild(icon);
-
-  return li;
-};
-
-const iterateTask = (tasks) => {
-  const tasksContainer = document.querySelector('.task-container');
+  tasks.sort((a, b) => a.index - b.index);
 
   tasks.forEach((task) => {
-    const taskItem = createTaskItem(task);
-    tasksContainer.appendChild(taskItem);
-  });
-};
+    const taskItem = document.createElement("li");
+    taskItem.classList.add("task-item", "flex-row");
 
-// Call iterateTask function after HTML has loaded
-window.addEventListener('DOMContentLoaded', () => {
-  iterateTask(tasks);
+    if (task.completed) {
+      taskItem.classList.add("task-completed");
+    }
+
+    const taskCheckbox = document.createElement("input");
+    taskCheckbox.type = "checkbox";
+    taskItem.appendChild(taskCheckbox);
+
+    const taskDescription = document.createElement("p");
+    taskDescription.innerText = task.description;
+    taskItem.appendChild(taskDescription);
+
+    const taskMenu = document.createElement("i");
+    taskMenu.classList.add("las", "la-ellipsis-v", "btn");
+    taskItem.appendChild(taskMenu);
+
+    taskContainer.appendChild(taskItem);
+  });
+}
+
+window.addEventListener("load", () => {
+  renderTasks();
 });
