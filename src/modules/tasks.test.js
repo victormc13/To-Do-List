03e106abbd6e0464b3ffbe5c38908b58686dd7ac
victormc13 <viktorsb14@gmail.com>
2023-05-25 11:19:20 -0500

    Add testing branch

--- src/modules/tasks.test.js
+++ src/modules/tasks.test.js
@@ -2,6 +2,6 @@ import { addTask } from './tasks'
 
 describe('addTask', () => {
   test('should add task when addTask class is called', () => {
-    expect(addTask(description)).toBe('task1')
+    
   })
 });
