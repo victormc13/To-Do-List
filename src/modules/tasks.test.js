10517b1e0cdd60ef5e1499d3dcf3e6f8d35f98ce
JuanDBta <juandavid_diaz@hotmail.com>
2023-05-25 12:44:42 -0500

    Add test for addTask and mock DOM

--- src/modules/tasks.test.js
+++ src/modules/tasks.test.js
@@ -1,7 +1,13 @@
 import { addTask } from './tasks'
 
 describe('addTask', () => {
-  test('should add task when addTask class is called', () => {
-    
+  test('should generate p element with task description when addTask is called', () => {
+    document.body.innerHTML =
+  '<div>' +
+  '  <ul class="task-container"><li></li></ul>' +
+  '</div>';
+  addTask();
+  const list = document.querySelectorAll('.task-item p');
+  expect(list).toHaveLength(1);
   })
 });
